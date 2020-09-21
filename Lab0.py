a40d1286e4a121baef519c22ed0c2344d776fe4f
vinrich <colin@vinarcik.com>
2020-09-20 21:48:15 -0400

    implemented a basic version of a custom neural net

--- Lab0.py
+++ Lab0.py
@@ -27,13 +27,13 @@ ALGORITHM = "custom_net"
 
 
 
-
+#(784, 10, 512);
 
 class NeuralNetwork_2Layer():
     def __init__(self, inputSize, outputSize, neuronsPerLayer, learningRate = 0.1):
-        self.inputSize = inputSize
-        self.outputSize = outputSize
-        self.neuronsPerLayer = neuronsPerLayer
+        self.inputSize = inputSize  #784
+        self.outputSize = outputSize  #10
+        self.neuronsPerLayer = neuronsPerLayer  #512
         self.lr = learningRate
         self.W1 = np.random.randn(self.inputSize, self.neuronsPerLayer)
         self.W2 = np.random.randn(self.neuronsPerLayer, self.outputSize)
@@ -53,13 +53,53 @@ class NeuralNetwork_2Layer():
 
     # Training with backpropagation.
     def train(self, xVals, yVals, epochs = 100000, minibatches = True, mbs = 100):
-        pass                                   #TODO: Implement backprop. allow minibatches. mbs should specify the size of each minibatch.
+        #training the model to make accurate predictions while adjusting weights continually
+        gen1 = self.__batchGenerator(xVals, mbs)
+        gen2 = self.__batchGenerator(yVals, mbs)
+
+        for iteration in range(mbs):
+            #siphon the training data via  the neuron
+            batchx = next(gen1)
+            print("testing x")
+            print(str(batchx.shape))
+            batchy = next(gen2)
+
+            print(str(batchy.shape))
+            
+            output = self.predict(batchx)
+
+            #print(output)
+            #computing error rate for back-propagation
+            error = batchy - output
+            print("# DEBUG: ")
+            print(str(batchy.shape))
+            print(str(output.shape))
+            print(str(error.shape))
+            delta = error * self.__sigmoidDerivative(output) # applying derivative of sigmoid to error
+            z2_error = delta.dot(self.W2.T) # z2 error: how much our hidden layer weights contributed to output error
+            z2_delta = z2_error*self.__sigmoidDerivative(self.layer1) # applying derivative of sigmoid to z2 error
+            #performing weight adjustments
+            print("testing change of weight")
+            print(str(z2_delta.shape))
+            print("testing x")
+            print(str(self.W1.shape))
+            print(str(batchx.T.shape))
+            self.W1 += batchx.T.dot(z2_delta) # adjusting first set (input --> hidden) weights
+            self.W2 += self.layer1.T.dot(delta) # adjusting second set (hidden --> output) weights
+
+                                          #TODO: Implement backprop. allow minibatches. mbs should specify the size of each minibatch.
+
+
+
 
     # Forward pass.
     def __forward(self, input):
-        layer1 = self.__sigmoid(np.dot(input, self.W1))
-        layer2 = self.__sigmoid(np.dot(layer1, self.W2))
-        return layer1, layer2
+        input = input.astype(float)
+        print(str(input.shape))
+        print(str(self.W1.shape))
+        self.layer1 = self.__sigmoid(np.dot(input, self.W1))
+        layer2 = self.__sigmoid(np.dot(self.layer1, self.W2))
+        return self.layer1, layer2
 
     # Predict.
     def predict(self, xVals):
@@ -96,21 +136,28 @@ def preprocessData(raw):
     ((xTrain, yTrain), (xTest, yTest)) = raw            #TODO: Add range reduction here (0-255 ==> 0.0-1.0).
     yTrainP = to_categorical(yTrain, NUM_CLASSES)
     yTestP = to_categorical(yTest, NUM_CLASSES)
-    print("New shape of xTrain dataset: %s." % str(xTrain.shape))
-    print("New shape of xTest dataset: %s." % str(xTest.shape))
+    xTrainP = xTrain.reshape((60000, 784))
+    xTestP = xTest.reshape((10000, 784))
+    print("New shape of xTrain dataset: %s." % str(xTrainP.shape))
+    print("New shape of xTest dataset: %s." % str(xTestP.shape))
     print("New shape of yTrain dataset: %s." % str(yTrainP.shape))
     print("New shape of yTest dataset: %s." % str(yTestP.shape))
-    return ((xTrain, yTrainP), (xTest, yTestP))
+    return ((xTrainP, yTrainP), (xTestP, yTestP))
 
 
 
 def trainModel(data):
+    NeuralNetwork = NeuralNetwork_2Layer(784, 10, 512);
     xTrain, yTrain = data
     if ALGORITHM == "guesser":
         return None   # Guesser has no model, as it is just guessing.
     elif ALGORITHM == "custom_net":
         print("Building and training Custom_NN.")
-        print("Not yet implemented.")                   #TODO: Write code to build and train your custon neural net.
+        print("implemented.")                   #TODO: Write code to build and train your custon neural net.
+        NeuralNetwork.train(xTrain, yTrain)
+        print("Ending Weights After Training: ")
+        print(neural_network.synaptic_weights)
+
         return None
     elif ALGORITHM == "tf_net":
         print("Building and training TF_NN.")
@@ -126,6 +173,7 @@ def runModel(data, model):
         return guesserClassifier(data)
     elif ALGORITHM == "custom_net":
         print("Testing Custom_NN.")
+        NeuralNetwork.predict(xTest, yTest)
         print("Not yet implemented.")                   #TODO: Write code to run your custon neural net.
         return None
     elif ALGORITHM == "tf_net":
